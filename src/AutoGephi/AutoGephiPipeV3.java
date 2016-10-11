1ed9889a5334556a6b114a4a776834b4dfee1964
LOyster1 <loyster1@alaska.edu>
2016-10-11 11:57:07 -0800

    updated to import single graph or directory

--- src/AutoGephi/AutoGephiPipeV3.java
+++ src/AutoGephi/AutoGephiPipeV3.java
@@ -1,5 +1,5 @@
 package AutoGephi;
-
+import java.util.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -51,10 +51,12 @@ public class AutoGephiPipeV3
     private static String processedFile;
     private static DynamicProcessor dynamicProcessor;
     private static Container container;
+    public static Scanner read;
+    public static String year, month,day;
     
     //fileDate is a regex for extracting the date of each imported file and uses it to append as TimeInterval
     private static String fileDate="^[a-zA-Z0-9/*]+((([0-1][0-9]{3})|([2][0][0-9]{2}))[-](([0][1-9])|([1][0-2]))[-](([0][1-9])|([1-2][0-9])|([3][0-1]))+).*";
-
+    public static String interiorDate="-?\\d+";
     
 
     
@@ -126,7 +128,7 @@ public class AutoGephiPipeV3
         
          //Run modularity algorithm - community detection
         Modularity modularity = new Modularity();
-        modularity.setResolution(1);
+        modularity.setResolution(0.15);
         modularity.execute(graphModel, attributeModel);
 
         //Partition with 'modularity_class', just created by Modularity algorithm
@@ -201,6 +203,57 @@ public class AutoGephiPipeV3
         
     }*/
     
+//    public static void importGraph(File file)//////imports graph and appends it to existing graph
+//    {
+//       // ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+//        //Container container;
+//        
+//        Pattern p=Pattern.compile(fileDate);//used to extract FileDate from imported graph
+//        try//place imported data into container
+//        {
+//            //NEED TO UPDATE TO IMPORT VIA COMMAND LINE
+//            //File file=Utilities.toFile(AutoGephiPipeV3.class.getResource(fileName).toURI());
+//           // File file=Utilities.toFile(fileName.toURI());
+//            String fileName=file.getName();
+//            
+//            System.out.println("File imported: " +file.toString());
+//            container = importController.importFile(file);
+//            container.getLoader().setEdgeDefault(EdgeDefault.UNDIRECTED);//set to undirected
+//            System.out.println(fileName+" was appended to graph.");
+//            
+//            String[] tokens = fileName.split("\\.(?=[^\\.]+$)");///Eliminate input extension for use in output name.
+//            processedFile=tokens[0];///Used for export file name
+//            //Searches file name for a proper date and then appends the date as a time interval
+//            Matcher m = p.matcher(fileName);
+//            if (m.find()) 
+//            {
+//                //Set date for this file
+//                System.out.println(fileName);
+//                System.out.println(m.group(1));
+//                dynamicProcessor.setDate(m.group(1));///Set time interval
+//                System.out.println("Date Set: " +dynamicProcessor.getDate());
+//            }
+//            else
+//            {
+//                System.out.println("invalid date");
+//            }   
+//              
+//            
+//            //Process the container using the DynamicProcessor
+//            importController.process(container, dynamicProcessor, workspace);
+//            
+//            
+//        }
+//        catch(Exception ex)
+//        {
+//           // System.out.println(fileName+" was NOT appended to graph.");
+//            ex.printStackTrace();
+//            return;
+//        }
+//        //Append imported data to GraphAPI
+//        //importController.process(container, new DefaultProcessor(), workspace);
+//        
+//    }
     public static void importGraph(File file)//////imports graph and appends it to existing graph
     {
        // ImportController importController = Lookup.getDefault().lookup(ImportController.class);
@@ -234,6 +287,70 @@ public class AutoGephiPipeV3
             else
             {
                 System.out.println("invalid date");
+                ///////Attempt to grab date from file itself
+                int count=0;
+                
+                try
+		{
+			//file=new File(file);
+			read=new Scanner(file);
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return;
+		}
+                while(read.hasNext())
+                {
+                    //System.out.println(read.next());
+                    String temp=read.next();
+                    p=Pattern.compile(interiorDate);
+                    m=p.matcher(temp);
+                    if(m.find())
+                    {
+                        //System.out.println(temp);
+                        count++;
+                        if(count==1 && temp.length()==4)
+                        {
+                            year=temp;
+                        }
+                        else if(count==2)
+                        {
+                            if(temp.length()==1)
+                            {
+                                temp="0"+temp;
+                                month=temp;
+                            }
+                            else
+                            {
+                                month=temp;
+                            }
+                            
+                        }
+                        else if(count==3)
+                        {
+                            if(temp.length()==1)
+                            {
+                                temp="0"+temp;
+                                day=temp;
+                            }
+                            else
+                            {
+                                day=temp;
+                            }
+                            break;
+                        }
+                       
+                    }
+                    
+                }
+                System.out.println(year+"-"+month+"-"+day);
+                String newName=year+"-"+month+"-"+day;
+                dynamicProcessor.setDate(newName);///Set time interval
+                System.out.println("Date Set: " +dynamicProcessor.getDate());
+                
+                
+                
             }   
               
             
@@ -252,19 +369,34 @@ public class AutoGephiPipeV3
         //importController.process(container, new DefaultProcessor(), workspace);
         
     }
+    
     public static void importDirectory(String dirName)
     {
-        File[] files = new File("../"+dirName).listFiles();
-        System.out.println(dirName);
-        //System.out.println(files.length);
-        
-        for(int i=0;i<files.length-1; i++)
-        { 
-            try {
-                //importGraph(files[i].getName());
-                importGraph(files[i]);
-            } catch (Exception e) {
-                System.out.println(files[i].toString() + " is not a graph file");
+        //File[] files = new File("../"+dirName).listFiles();
+        File tempFile=new File(dirName);
+        if(tempFile.isDirectory()==true)
+        {
+            File[] files = new File(dirName).listFiles();
+            System.out.println(dirName);
+            //System.out.println(files.length);
+
+            for (int i = 0; i <= files.length - 1; i++) {
+                try {
+                    //importGraph(files[i].getName());
+                    importGraph(files[i]);
+                } catch (Exception e) {
+                    System.out.println(files[i].toString() + " is not a graph file");
+                }
+            }
+        }
+        else if(tempFile.isFile()==true)
+        {
+            try
+            {
+                importGraph(tempFile);
+            }
+            catch(Exception e){
+                e.printStackTrace();
             }
         }
     } 
